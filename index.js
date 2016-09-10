29af0eb8a667d43dbb5b7a8d9e9b364c4ac80ad7
Roy Chung <rthug7@gmail.com>
2016-09-10 00:55:02 -0700

    index.js add some features commit #2

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var io =  require('socket.io')(server);
 
 var username;
 var userList = {};
+
 var numUsers = 0;
+
 
 server.listen(3000, function(){
   console.log('App is listening on port 3000...');
@@ -17,41 +19,50 @@ io.on('connection', function(socket){
     
     
     socket.on('add user', function(username){
-        
+//        socket.color = color;
         socket.userNick = username;
         userList[socket.id] = username;
         console.log(userList);
+        
         io.emit('userlist', userList);
         io.emit('greeting', username + " just joined the chat!");
-        console.log(username + " joined the chat");
+        console.log(username  + " joined the chat");
+        
         ++numUsers;
         console.log(numUsers + " are in the chat");
     });
     
-    
+    //When the server gets the message from the client, 
+    //the server resends the message to the client with 
+    //the sender name.
     socket.on('sendmsg',function(msg){
         io.emit('chat', userList[socket.id], msg);
         console.log(userList[socket.id] + " says: " + msg);
     });
     
-    
-    
-    
-    
-    
-    
     socket.on('disconnect', function(){
-        username = userList[socket.id];
-        delete userList[socket.id];
-        io.emit('userlist', userList);
-        console.log(username + ' left the chat room');
         
-        
-        --numUsers;
         if(numUsers === 0){
             console.log("No one in the chat!");
-        }else{
+        }else if(numUsers >= 0){
+            
+            username = userList[socket.id];
+        
+            //When the user disconnects, emit bye message to all clients.
+            io.emit('bye', username + " just left the chat!");
+
+            //Remove the user from the global list
+            delete userList[socket.id];
+
+            //Emit new global list to client
+            io.emit('userlist', userList);
+            
+            console.log(username + ' left the chat room');
+            --numUsers;
             console.log(numUsers + " is/are in the chat");
+            console.log(userList);
+        }else{
+            console.log("number cannot be negative")
         }
     });
 });
